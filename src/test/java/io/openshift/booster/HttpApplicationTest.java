84030cfdf45d09459b38f29e0807dd4373519927
vertexavier <javier.vargas.27@gmail.com>
2018-01-30 01:31:24 +0000

    Cambios efectuados

--- src/test/java/io/openshift/booster/HttpApplicationTest.java
+++ src/test/java/io/openshift/booster/HttpApplicationTest.java
@@ -41,7 +41,7 @@ public class HttpApplicationTest {
                 context.assertTrue(resp.succeeded());
                 context.assertEquals(resp.result().statusCode(), 200);
                 String content = resp.result().bodyAsJsonObject().getString("content");
-                context.assertEquals(content, String.format(template, "World"));
+                context.assertEquals(content, String.format(template, "Hola Mundo"));
                 async.complete();
             });
     }
